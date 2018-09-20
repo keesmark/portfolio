fad0fd42164c0da492a81142f40dded32d75d27f
Katsuhisa Hata <keesmark@gmail.com>
2018-09-20 16:08:15 -0600

    main_slide

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.slick-box').slick({
+		arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    speed: 5000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+ 　});
+});
