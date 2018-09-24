b46a242641c0fa3f669d0ad15e4f57c724a25b94
Katsuhisa Hata <keesmark@gmail.com>
2018-09-24 09:24:46 -0600

    toppage

--- js/main.js
+++ js/main.js
@@ -9,3 +9,40 @@ $(function() {
     slidesToScroll: 1,
  　});
 });
+
+$(window).scroll(function () {
+
+  if ($(this).scrollTop() > 10) {
+    $('header').addClass('is-fixed');
+    } else {
+    　	$('header').removeClass('is-fixed');
+    }
+});
+
+$(function() {
+    $("#nav_btn").click(function () {
+	  	$("#nav_container").fadeIn("slow");
+	});
+    $("#close").on("click",function(){
+	  $("#nav_container").fadeOut("slow");  
+	});
+});
+
+function updateButton() {
+    if ($(this).scrollTop() >= 300) { 
+        $(".top").fadeIn();
+    } else {
+        $(".top").fadeOut();
+    }
+}
+
+$(document).ready(function() {
+    updateButton();
+    $(window).scroll(updateButton);
+    $(".top").click(function() {
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
+        return false;
+    });
+});
