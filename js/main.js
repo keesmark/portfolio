d097ea4c3ee9adc3d0114f3e543e4f7f403b041d
Katsuhisa Hata <keesmark@gmail.com>
2018-10-02 00:29:08 -0600

    fadein

--- js/main.js
+++ js/main.js
@@ -97,3 +97,16 @@ $(function(){
         }
     };
 });
+
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
